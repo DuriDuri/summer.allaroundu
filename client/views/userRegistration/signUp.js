64860dbbe3fdaa72447967e11ae947634efeb764
Duri Duri <durid@union.edu>
2015-08-04 12:04:40 -0400

    Add home page

--- client/views/userRegistration/signUp.js
+++ client/views/userRegistration/signUp.js
@@ -54,6 +54,46 @@ Template.signUp.events({
 
 
 
+Template.signUp.helpers({
+	
+	// Map options
+	mapOptions: function() {
+        if (GoogleMaps.loaded()) {
+            return {
+                center: new google.maps.LatLng(42.3714947, -71.0864239),
+                draggable: false/true,
+                scrollwheel: false,
+                zoom: 11,
+                backgroundColor: 'rgb(27,28,29)',
+                color: 'rgb(27,28,29)',
+                styles : [
+				  {
+				    stylers: [
+				      { hue: "#00ffe6" },
+				      { saturation: -20 }
+				    ]
+				  },{
+				    featureType: "road",
+				    elementType: "geometry",
+				    stylers: [
+				      { lightness: 100 },
+				      { visibility: "simplified" }
+				    ]
+				  },{
+				    featureType: "road",
+				    elementType: "labels",
+				    stylers: [
+				      { visibility: "off" }
+				    ]
+				  }
+				]
+				            };
+				        }
+				    }
+				});
+
+
+
 
 
 
@@ -135,6 +175,34 @@ Template.signUp.onRendered( function(){
 	     		}
 	     	}
 	});
+
+
+	// Initiate the map
+	GoogleMaps.load();
+
+	// Initiate geocomplete autorun
+	this.autorun(function () {
+		// Wait for API to be loaded
+		if (GoogleMaps.loaded()) {
+
+        var options = {
+          map: ".map-container"
+        };
+
+        // Example 2 - Autocomplete + map
+        $('#location').geocomplete(options)
+          .bind("geocode:result", function(event, result){
+            console.log("Result: " + result.geometry.location);
+          })
+
+
+      }
+    });
+
+
+
+
+// End of onRendered
 });
 
 
