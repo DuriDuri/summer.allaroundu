020aaef89af641f69957d422b526ac8450984b4c
Duri Duri <durid@union.edu>
2015-08-07 14:48:33 -0400

    One marker at a time

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -25,6 +25,27 @@ Template.home.onRendered(function() {
                 google.maps.event.addListener(marker, 'click', function() {
                     
                     infoWindow.open(map.instance, marker);
+                    google.maps.Map.prototype.markers = new Array();
+
+					google.maps.Map.prototype.getMarkers = function() {
+					    return this.markers
+					};
+
+					google.maps.Map.prototype.clearMarkers = function() {
+					    for(var i=0; i<this.markers.length; i++){
+					        this.markers[i].setMap(null);
+					    }
+					    this.markers = new Array();
+					};
+
+					google.maps.Marker.prototype._setMap = google.maps.Marker.prototype.setMap;
+
+					google.maps.Marker.prototype.setMap = function(map) {
+					    if (map) {
+					        map.markers[map.markers.length] = this;
+					    }
+					    this._setMap(map);
+					}
                 }); 
             }
         })
